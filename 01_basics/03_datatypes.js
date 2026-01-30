1e9467a7cf76302af8d4c1b74a810c458b1b353f
Ayush Kurhade <140232317+AyushKurhade@users.noreply.github.com>
2026-01-30 08:52:06 +0000

    datatypes in javascript

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Welcome to Datatypes in JavaScript");
+ // we are using node js not browser
+
+console.log(3+3); 
+console.log("Ayush")
+
+let name = "Anjali"
+let dob = 20605
+let isStupid = true
+ 
+// number 
+// string = ""
+// boolean = true/ false 
+// null=> standalone value
+// undefined
+// symbol
+// object 
+
+console.log(typeof name);
+console.log(typeof null); // object
+console.log(typeof dob);
+console.log(typeof undefined);
+
