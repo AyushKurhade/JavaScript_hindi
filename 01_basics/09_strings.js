9af08a96d609db7b76ff93bb47f392970cc25c0a
Ayush Kurhade <140232317+AyushKurhade@users.noreply.github.com>
2026-02-02 16:55:14 +0000

    strings : implement template literals, substring, trim, replace, includes etc

--- 01_basics/09_strings.js
+++ 01_basics/09_strings.js
@@ -1,35 +1,36 @@
-const name = "hitesh"
-const repoCount = 50
+ const name = "Ayush"
+ const age = 20 ;
 
-// console.log(name + repoCount + " Value");
+ console.log(name + " " + age +" ");
+ 
+// Template Literal
+ console.log(`Hello my Name is ${name} and my age is ${age}`);
+ 
+// String Manupulation
+ const s1 = new String('AyushK')
+ console.log(s1.length);
+ console.log(s1.toUpperCase());
+ console.log(s1.charAt(2));
+ console.log(s1.indexOf('A'));
 
-console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+//  substring
+ const s2 = s1.substring(0 , 2)
+ console.log(s2);
+ 
+//  Slice
+ const s3 = s1.slice(-6,4)
+ console.log(s3);
 
-const gameName = new String('hitesh-hc-com')
+//  Trim        https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/trim
+ const S = "   Ayush    "
+ console.log(S);
+console.log(S.trim());
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+// Replace and includes
+const url = "https://www.ayushk30%30/discrod.com"
+console.log(url.replace('%30' , '-'));
+console.log(url.includes('ayush'));
+ 
 
 
-// console.log(gameName.length);
-// console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
-
-const newString = gameName.substring(0, 4)
-console.log(newString);
-
-const anotherString = gameName.slice(-8, 4)
-console.log(anotherString);
-
-const newStringOne = "   hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
-
-const url = "https://hitesh.com/hitesh%20choudhary"
-
-console.log(url.replace('%20', '-'))
-
-console.log(url.includes('sundar'))
-
-console.log(gameName.split('-'));
+ 
