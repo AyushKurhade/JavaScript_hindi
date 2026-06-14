1aed975701e19dd7cbc0027581ec4d54b820f177
Ayush Kurhade <aayushkurhade068@gmail.com>
2026-06-14 22:28:35 +0530

    Add JavaScript for...in loop examples with objects and arrays

---
+++ 05_iterations/04_forInLoop.js
@@ -0,0 +1,46 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+// ******************************************
+
+// Syantax 
+// const object = { a: 1, b: 2, c: 3 };
+
+// for (const property in object) {
+//   console.log(`${property}: ${object[property]}`);
+// }
+
+// Expected output:
+// "a: 1"
+// "b: 2"
+// "c: 3"
+
+// featured for :
+// Objects (Key-Value entries)
+
+
+
+// *************************************
+for (const key in myObject) {
+    console.log(`${key} shortcut is for ${myObject[key]}`);
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programming) {
+    //console.log(programming[key]);
+}
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('Fr', "France")
+// map.set('IN', "India")
+
+// for (const key in map) {
+//     console.log(key);
+// }
